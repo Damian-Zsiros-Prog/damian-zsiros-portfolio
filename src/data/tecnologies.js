bdc0aefbc26a1f457c18b4cfca7414fee9627ca8
Damian Zsiros <damjeronzalez23@gmail.com>
2023-03-14 23:33:11 -0500

    add tecnologies only with names

---
+++ src/data/tecnologies.js
@@ -0,0 +1,34 @@
+export const tecnologies = [
+  {
+    name: 'React Js',
+    pathLogo: ''
+  },
+  {
+    name: 'Node Js',
+    pathLogo: ''
+  },
+  {
+    name: 'Astro',
+    pathLogo: ''
+  },
+  {
+    name: 'MongoDB',
+    pathLogo: ''
+  },
+  {
+    name: 'MySQL',
+    pathLogo: ''
+  },
+  {
+    name: 'PHP',
+    pathLogo: ''
+  },
+  {
+    name: 'VB.NET',
+    pathLogo: ''
+  },
+  {
+    name: 'Java',
+    pathLogo: ''
+  }
+]
